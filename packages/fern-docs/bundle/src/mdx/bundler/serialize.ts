8b6d610378300f7e36e12ef4da8bbef148e789f5
chdeskur <chdeskur@gmail.com>
2025-03-25 11:30:10 -0400

    init

--- packages/fern-docs/bundle/src/mdx/bundler/serialize.ts
+++ packages/fern-docs/bundle/src/mdx/bundler/serialize.ts
@@ -24,6 +24,7 @@ import remarkGemoji from "remark-gemoji";
 import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+import remarkShikiTwoslash from "remark-shiki-twoslash";
 import remarkSmartypants from "remark-smartypants";
 import remarkSqueezeParagraphs from "remark-squeeze-paragraphs";
 import { noop } from "ts-essentials";
@@ -177,7 +178,7 @@ async function serializeMdxImpl(
         remarkSmartypants,
         remarkMath,
         remarkGemoji,
-        remarkTwoslash,
+        [remarkShikiTwoslash, {}],
       ];
 
       const rehypePlugins: PluggableList = [
