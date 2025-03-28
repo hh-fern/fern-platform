bdbd2825292004e83a5a32b7df53aade17c3c72c
chdeskur <chdeskur@gmail.com>
2025-03-28 11:40:58 -0400

    remove external package

--- packages/fern-docs/bundle/next.config.ts
+++ packages/fern-docs/bundle/next.config.ts
@@ -210,7 +210,7 @@ const nextConfig: NextConfig = {
     })),
     path: cdnUri != null ? `${cdnUri.href}_next/image` : undefined,
   },
-  serverExternalPackages: ["vscode-oniguruma", "esbuild", "@typescript/vfs"],
+  serverExternalPackages: ["esbuild", "@typescript/vfs"],
   webpack: (config, { isServer }) => {
     // config.optimization = {
     //   ...config.optimization,
@@ -219,7 +219,6 @@ const nextConfig: NextConfig = {
     if (isServer) {
       config.externals = config.externals || [];
       config.externals.push("esbuild");
-      config.externals.push("vscode-oniguruma");
       config.externals.push("@typescript/vfs");
     }
     config.resolve.fallback = {
