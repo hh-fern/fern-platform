6e2a1403c8f2e7007ccddf8247c9ae201bfea65e
Deep Singhvi <deep@buildwithfern.com>
2025-04-08 14:43:27 -0400

    more things

--- packages/fern-docs/bundle/src/mdx/bundler/shiki/twoslashRenderer.ts
+++ packages/fern-docs/bundle/src/mdx/bundler/shiki/twoslashRenderer.ts
@@ -67,237 +67,259 @@ export function twoslashRenderer(): TwoslashRenderer {
 
   return {
     nodeStaticInfo(info, node) {
-      const themedContent = hightlightPopupContent(
-        this.codeToHast,
-        this.options,
-        info
-      );
-
-      if (!themedContent.length) return node;
-
-      return {
-        type: "element",
-        tagName: "span",
-        properties: {
-          class: "twoslash-hover",
-        },
-        children: [
-          {
-            type: "element",
-            tagName: "div",
-            properties: {
-              class: "twoslash-popup-info-hover",
-            },
-            children: themedContent,
-          },
-          {
-            type: "element",
-            tagName: "span",
-            properties: {
-              class: "twoslash-target",
-            },
-            children: [node],
-          },
-        ],
-      };
-    },
-
-    nodeQuery(info, node) {
-      if (!info.text) return {};
-
-      const themedContent = hightlightPopupContent(
-        this.codeToHast,
-        this.options,
-        info
-      );
-
-      return {
-        type: "element",
-        tagName: "span",
-        properties: {
-          class: "twoslash-query-persisted",
-        },
-        children: [
-          {
-            type: "element",
-            tagName: "span",
-            properties: {
-              class: "twoslash-popup-info",
-            },
-            children: [
-              {
-                type: "element",
-                tagName: "div",
-                properties: { class: "twoslash-popup-arrow" },
-                children: [],
-              },
-              {
-                type: "element",
-                tagName: "div",
-                properties: { class: "twoslash-popup-scroll-container" },
-                children: themedContent,
-              },
-            ],
-          },
-          node,
-        ],
-      };
-    },
-
-    nodeCompletion(query, node) {
-      if (node.type !== "text")
-        throw new Error(
-          `[shiki-twoslash] nodeCompletion only works on text nodes, got ${node.type}`
+      try {
+        const themedContent = hightlightPopupContent(
+          this.codeToHast,
+          this.options,
+          info
         );
 
-      const leftPart = query.completionsPrefix || "";
-      const rightPart = node.value.slice(leftPart.length || 0);
+        if (!themedContent.length) return node;
 
-      return {
-        type: "element",
-        tagName: "span",
-        properties: {},
-        children: [
-          {
-            type: "text",
-            value: leftPart,
-          },
-          {
-            type: "element",
-            tagName: "span",
-            properties: {
-              class: "twoslash-completion-cursor",
-            },
-            children: [
-              {
-                type: "element",
-                tagName: "div",
-                properties: {
-                  class: "twoslash-completion-list",
-                },
-                children: query.completions.map((i) => ({
-                  type: "element",
-                  tagName: "div",
-                  properties: {
-                    class: "twoslash-completion-list-item",
-                  },
-                  children: [
-                    {
-                      type: "element",
-                      tagName: "span",
-                      properties: {
-                        class:
-                          "kindModifiers" in i &&
-                          typeof i.kindModifiers === "string" &&
-                          i.kindModifiers?.split(",").includes("deprecated")
-                            ? "deprecated"
-                            : undefined,
-                      },
-                      children: [
-                        {
-                          type: "element",
-                          tagName: "span",
-                          properties: { class: "twoslash-completions-matched" },
-                          children: [
-                            {
-                              type: "text",
-                              value: i.name.startsWith(query.completionsPrefix)
-                                ? query.completionsPrefix
-                                : "",
-                            },
-                          ],
-                        },
-                        {
-                          type: "element",
-                          tagName: "span",
-                          properties: {
-                            class: "twoslash-completions-unmatched",
-                          },
-                          children: [
-                            {
-                              type: "text",
-                              value: i.name.startsWith(query.completionsPrefix)
-                                ? i.name.slice(
-                                    query.completionsPrefix.length || 0
-                                  )
-                                : i.name,
-                            },
-                          ],
-                        },
-                      ],
-                    },
-                  ],
-                })),
-              },
-            ],
-          },
-          {
-            type: "text",
-            value: rightPart,
-          },
-        ],
-      };
-    },
-
-    nodeError(_, node) {
-      return {
-        type: "element",
-        tagName: "span",
-        properties: {
-          class: "twoslash-error",
-        },
-        children: [node],
-      };
-    },
-
-    lineError(error) {
-      return [
-        {
-          type: "element",
-          tagName: "div",
-          properties: {
-            class: "twoslash-meta-line twoslash-error-line",
-          },
-          children: [
-            {
-              type: "text",
-              value: error.text,
-            },
-          ],
-        },
-      ];
-    },
-
-    lineCustomTag(tag) {
-      return [
-        {
-          type: "element",
-          tagName: "div",
-          properties: {
-            class: `twoslash-tag-line twoslash-tag-${tag.name}-line`,
-          },
-          children: [
-            {
-              type: "text",
-              value: tag.text || "",
-            },
-          ],
-        },
-      ];
-    },
-
-    nodesHighlight(_, nodes) {
-      return [
-        {
+        return {
           type: "element",
           tagName: "span",
           properties: {
-            class: "twoslash-highlighted",
+            class: "twoslash-hover",
           },
-          children: nodes,
-        },
-      ];
+          children: [
+            {
+              type: "element",
+              tagName: "div",
+              properties: {
+                class: "twoslash-popup-info-hover",
+              },
+              children: themedContent,
+            },
+            {
+              type: "element",
+              tagName: "span",
+              properties: {
+                class: "twoslash-target",
+              },
+              children: [node],
+            },
+          ],
+        };
+      } catch {
+        return node;
+      }
     },
+
+    // nodeQuery(info, node) {
+    //   try {
+    //     if (!info.text) return {};
+
+    //     const themedContent = hightlightPopupContent(
+    //       this.codeToHast,
+    //       this.options,
+    //       info
+    //     );
+
+    //     return {
+    //       type: "element",
+    //       tagName: "span",
+    //       properties: {
+    //         class: "twoslash-query-persisted",
+    //       },
+    //       children: [
+    //         {
+    //           type: "element",
+    //           tagName: "span",
+    //           properties: {
+    //             class: "twoslash-popup-info",
+    //           },
+    //           children: [
+    //             {
+    //               type: "element",
+    //               tagName: "div",
+    //               properties: { class: "twoslash-popup-arrow" },
+    //               children: [],
+    //             },
+    //             {
+    //               type: "element",
+    //               tagName: "div",
+    //               properties: { class: "twoslash-popup-scroll-container" },
+    //               children: themedContent,
+    //             },
+    //           ],
+    //         },
+    //         node,
+    //       ],
+    //     };
+    //   } catch {
+    //     return node;
+    //   }
+    // },
+
+    // nodeCompletion(query, node) {
+    //   try {
+    //     if (node.type !== "text")
+    //       throw new Error(
+    //         `[shiki-twoslash] nodeCompletion only works on text nodes, got ${node.type}`
+    //       );
+
+    //     const leftPart = query.completionsPrefix || "";
+    //     const rightPart = node.value.slice(leftPart.length || 0);
+
+    //     return {
+    //       type: "element",
+    //       tagName: "span",
+    //       properties: {},
+    //       children: [
+    //         {
+    //           type: "text",
+    //           value: leftPart,
+    //         },
+    //         {
+    //           type: "element",
+    //           tagName: "span",
+    //           properties: {
+    //             class: "twoslash-completion-cursor",
+    //           },
+    //           children: [
+    //             {
+    //               type: "element",
+    //               tagName: "div",
+    //               properties: {
+    //                 class: "twoslash-completion-list",
+    //               },
+    //               children: query.completions.map((i) => ({
+    //                 type: "element",
+    //                 tagName: "div",
+    //                 properties: {
+    //                   class: "twoslash-completion-list-item",
+    //                 },
+    //                 children: [
+    //                   {
+    //                     type: "element",
+    //                     tagName: "span",
+    //                     properties: {
+    //                       class:
+    //                         "kindModifiers" in i &&
+    //                         typeof i.kindModifiers === "string" &&
+    //                         i.kindModifiers?.split(",").includes("deprecated")
+    //                           ? "deprecated"
+    //                           : undefined,
+    //                     },
+    //                     children: [
+    //                       {
+    //                         type: "element",
+    //                         tagName: "span",
+    //                         properties: {
+    //                           class: "twoslash-completions-matched",
+    //                         },
+    //                         children: [
+    //                           {
+    //                             type: "text",
+    //                             value: i.name.startsWith(
+    //                               query.completionsPrefix
+    //                             )
+    //                               ? query.completionsPrefix
+    //                               : "",
+    //                           },
+    //                         ],
+    //                       },
+    //                       {
+    //                         type: "element",
+    //                         tagName: "span",
+    //                         properties: {
+    //                           class: "twoslash-completions-unmatched",
+    //                         },
+    //                         children: [
+    //                           {
+    //                             type: "text",
+    //                             value: i.name.startsWith(
+    //                               query.completionsPrefix
+    //                             )
+    //                               ? i.name.slice(
+    //                                   query.completionsPrefix.length || 0
+    //                                 )
+    //                               : i.name,
+    //                           },
+    //                         ],
+    //                       },
+    //                     ],
+    //                   },
+    //                 ],
+    //               })),
+    //             },
+    //           ],
+    //         },
+    //         {
+    //           type: "text",
+    //           value: rightPart,
+    //         },
+    //       ],
+    //     };
+    //   } catch {
+    //     return node;
+    //   }
+    // },
+
+    // nodeError(_, node) {
+    //   try {
+    //     return {
+    //       type: "element",
+    //       tagName: "span",
+    //       properties: {
+    //         class: "twoslash-error",
+    //       },
+    //       children: [node],
+    //     };
+    //   } catch {
+    //     return node;
+    //   }
+    // },
+
+    // lineError(error) {
+    //   return [
+    //     {
+    //       type: "element",
+    //       tagName: "div",
+    //       properties: {
+    //         class: "twoslash-meta-line twoslash-error-line",
+    //       },
+    //       children: [
+    //         {
+    //           type: "text",
+    //           value: error.text,
+    //         },
+    //       ],
+    //     },
+    //   ];
+    // },
+
+    // lineCustomTag(tag) {
+    //   return [
+    //     {
+    //       type: "element",
+    //       tagName: "div",
+    //       properties: {
+    //         class: `twoslash-tag-line twoslash-tag-${tag.name}-line`,
+    //       },
+    //       children: [
+    //         {
+    //           type: "text",
+    //           value: tag.text || "",
+    //         },
+    //       ],
+    //     },
+    //   ];
+    // },
+
+    // nodesHighlight(_, nodes) {
+    //   return [
+    //     {
+    //       type: "element",
+    //       tagName: "span",
+    //       properties: {
+    //         class: "twoslash-highlighted",
+    //       },
+    //       children: nodes,
+    //     },
+    //   ];
+    // },
   };
 }
 
