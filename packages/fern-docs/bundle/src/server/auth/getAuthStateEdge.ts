42f34ca90d8b97debf6256018b8a24244f740166
Deep Singhvi <deep@buildwithfern.com>
2025-04-29 13:57:58 -0400

    Revert "fix: parse preview url org (#2634)"
    This reverts commit cb65b88c71facacd234fc0fc97992a3bc9126763.

--- packages/fern-docs/bundle/src/server/auth/getAuthStateEdge.ts
+++ packages/fern-docs/bundle/src/server/auth/getAuthStateEdge.ts
@@ -1,7 +1,6 @@
 import { NextRequest } from "next/server";
 
 import { COOKIE_FERN_TOKEN } from "@fern-docs/utils";
-import { extractOrgFromPreview } from "@fern-docs/utils";
 
 import { getDocsDomainEdge } from "../xfernhost/edge";
 import { createGetAuthState } from "./getAuthState";
@@ -16,17 +15,12 @@ export async function createGetAuthStateEdge(
 ): ReturnType<typeof createGetAuthState> {
   const domain = getDocsDomainEdge(request);
   const fern_token = request.cookies.get(COOKIE_FERN_TOKEN)?.value;
-
-  // extract org from preview domain
-  const org = extractOrgFromPreview(domain);
-  const orgMetadata = org ? { org, isPreview: true } : undefined;
-
   return createGetAuthState(
     request.nextUrl.host,
     domain,
     fern_token,
     undefined,
-    orgMetadata,
+    undefined,
     setFernToken
   );
 }
--- packages/fern-docs/edge-config/src/index.ts
+++ packages/fern-docs/edge-config/src/index.ts
@@ -2,9 +2,6 @@ export { getAuthEdgeConfig } from "./getAuthEdgeConfig";
 export { getCustomerAnalytics } from "./getCustomerAnalytics";
 export { getEdgeFlags } from "./getEdgeFlags";
 export { getLaunchDarklySettings } from "./getLaunchDarklySettings";
-export {
-  getPreviewUrlAuthConfig,
-  type PreviewUrlAuth,
-} from "./getPreviewUrlAuthConfig";
+export { getPreviewUrlAuthConfig } from "./getPreviewUrlAuthConfig";
 export { getSeoDisabled } from "./getSeoConfig";
 export { getCanonicalUrl } from "./getCanonicalUrl";
