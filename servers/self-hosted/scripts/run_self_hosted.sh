7a30b7ef7a124c9823a057078efab619401cdd51
Ariel <ariel@Ariels-MacBook-Pro.local>
2025-05-27 17:17:44 -0400

    generate docs

--- servers/self-hosted/scripts/run_self_hosted.sh
+++ servers/self-hosted/scripts/run_self_hosted.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+if [ ! -d "/app/fern" ]; then
+    echo "Fern folder not found. Please ensure you are mounting yours in."
+    exit 1
+fi
+
 source /app/servers/self-hosted/.env
 
 # -----------  Start run Postgres  -----------
@@ -81,6 +86,20 @@ done
 echo "FDR is up and running at localhost:8080"
 
 # -----------  Finish run FDR  -----------
+
+
+# hmmmm ok now we need someones files to be in here and we then use them to generate the docs
+# fern CLI was already installed in the docker image
+# need to make sure this hits the right port and that the files get inserted in our postgres db
+cd /app/
+
+if [ -d "fern" ]; then
+    echo "Found fern folder in current directory"
+else
+    echo "fern folder NOT found in current directory"
+fi
+
+fern generate --docs
 
 if [ "${RUN_MODE:-}" = "shell" ]; then
     echo "Entering shell mode..."
