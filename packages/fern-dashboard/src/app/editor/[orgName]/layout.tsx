8785ad2b16cd8236ac07a267e91a140f811ca4ca
Sarah Bawabe <sarah@buildwithfern.com>
2025-05-30 12:47:19 -0400

    feat(dash): initialize supabase

---
+++ packages/fern-dashboard/src/app/editor/[orgName]/layout.tsx
@@ -0,0 +1,15 @@
+import { ProtectedRoute } from "@/components/auth/ProtectedRoute";
+
+import { Auth0OrgName } from "../../services/auth0/types";
+
+export default async function AuthedLayout({
+  params,
+  children,
+}: Readonly<{
+  params: Promise<{ orgName: Auth0OrgName }>;
+  children: React.JSX.Element;
+}>) {
+  const { orgName } = await params;
+
+  return <ProtectedRoute orgName={orgName}>{children}</ProtectedRoute>;
+}
