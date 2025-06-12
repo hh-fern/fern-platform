3a765c836f4f1c8cf1db7b821f833d550df1bfe6
andy <andy@fernbook.local>
2025-06-12 14:37:13 -0400

    type github repo

--- packages/fern-dashboard/src/app/api/get-user-github-repos/handler.ts
+++ packages/fern-dashboard/src/app/api/get-user-github-repos/handler.ts
@@ -2,6 +2,7 @@ import { Octokit } from "@octokit/core";
 
 import * as auth0Management from "@/app/services/auth0/management";
 import { Auth0UserID } from "@/app/services/auth0/types";
+import { GithubRepo } from "@/app/services/github/types";
 
 export default async function getUserGithubRepos(userId: Auth0UserID) {
   const githubToken = await auth0Management.getUserGithubToken(userId);
@@ -14,5 +15,5 @@ export default async function getUserGithubRepos(userId: Auth0UserID) {
     url: repo.html_url,
   }));
 
-  return repos;
+  return repos as GithubRepo[];
 }
