67f49fcc876ce50cfa251ebdbde33563249b5d9d
Gage-K <krausegage@gmail.com>
2025-06-19 14:58:37 -0400

    (chore): add gap between mobile sidebar links [FER-5473] - hotfix for previous pr that had unrelated changes from another pr - added gap (2) between links in mobile sidebar header; smaller gap looked better than large gap sizes - made parente div a flex-column container

--- packages/fern-docs/bundle/src/components/sidebar/MobileSidebarHeaderLinks.tsx
+++ packages/fern-docs/bundle/src/components/sidebar/MobileSidebarHeaderLinks.tsx
@@ -15,7 +15,7 @@ export function MobileSidebarHeaderLinks({
   return (
     <div
       className={cn(
-        "border-border-concealed -mx-4 mt-4 list-none border-t p-4 [&>*]:flex",
+        "border-border-concealed -mx-4 mt-4 flex list-none flex-col gap-2 border-t p-4 [&>*]:flex",
         { "lg:hidden": hideInDesktop }
       )}
     >
