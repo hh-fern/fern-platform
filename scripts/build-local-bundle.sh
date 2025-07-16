f7aa68805825c521652c7bbde1344c90330d6db8
Catherine Deskur <support@buildwithfern.com>
2025-07-16 14:56:21 -0400

    interim

--- scripts/build-local-bundle.sh
+++ scripts/build-local-bundle.sh
@@ -6,7 +6,7 @@ find packages/fern-docs/bundle/.next -depth -mindepth 1 -not -path "packages/fer
 # find .next -depth -mindepth 1 -not -path ".next/standalone*" -exec rm -rf {} \;
 # rm -rf .next/standalone/node_modules/.pnpm/esbuild@0.25.0 && rm -rf .next/standalone/node_modules/.pnpm/@esbuild+linux-x64@0.25.0
 # cd ../../../
-# tar --no-xattrs -czf docs_bundle.tar.gz -C packages/fern-docs/bundle/.next/standalone .
+tar --no-xattrs -czf docs_bundle_2.tar.gz -C packages/fern-docs/bundle/.next/standalone .
 # Note: The following PowerShell Compress-Archive command (for Windows) will NOT preserve symlinks; 
 # any symlinks in the source directory will be included as regular files/directories (full copies of the targets).
-powershell -Command "Compress-Archive -Path packages/fern-docs/bundle/.next/standalone\* -DestinationPath docs_bundle.zip"
+# powershell -Command "Compress-Archive -Path packages/fern-docs/bundle/.next/standalone\* -DestinationPath docs_bundle.zip"
