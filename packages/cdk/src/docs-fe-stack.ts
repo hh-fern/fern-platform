fd227e9584a2867b706751a1238009cdde8b1f3c
Catherine Deskur <support@buildwithfern.com>
2025-07-17 21:45:36 -0400

    remove complex zipping

--- packages/cdk/src/docs-fe-stack.ts
+++ packages/cdk/src/docs-fe-stack.ts
@@ -83,18 +83,18 @@ async function zipFolder(sourceFolder: string, zipFilePath: string) {
 
   return new Promise<void>((resolve, reject) => {
     const output = fs.createWriteStream(zipFilePath);
-    // const archive = archiver("tar", {
-    //   gzip: true,
-    // });
-    const useDereference = process.platform === "win32";
-    if (useDereference) {
-      // eslint-disable-next-line no-console
-      console.debug("[zipFolder] Running in dereference mode for archiver (win32 platform)");
-    }
-    const archive = archiver("tar", useDereference
-      ? { gzip: true, dereference: true } as any
-      : { gzip: true }
-    );
+    const archive = archiver("tar", {
+      gzip: true,
+    });
+    // const useDereference = process.platform === "win32";
+    // if (useDereference) {
+    //   // eslint-disable-next-line no-console
+    //   console.debug("[zipFolder] Running in dereference mode for archiver (win32 platform)");
+    // }
+    // const archive = archiver("tar", useDereference
+    //   ? { gzip: true, dereference: true } as any
+    //   : { gzip: true }
+    // );
 
     archive.on("error", (err: unknown) => {
       reject(err instanceof Error ? err : new Error(String(err)));
