a6b22fad05f3bd038af01c4a5394292d20894abf
jsklan <jsklan.development@gmail.com>
2025-08-21 14:51:20 -0400

    revert script changes

--- scripts/build-local-bundle.sh
+++ scripts/build-local-bundle.sh
@@ -1,12 +1,4 @@
-cross-env NODE_ENV=production pnpm --filter=@fern-docs/bundle docs:build:local
+NODE_ENV=production pnpm --filter=@fern-docs/bundle docs:build:local
 cp -r packages/fern-docs/bundle/.next/static packages/fern-docs/bundle/.next/standalone/packages/fern-docs/bundle/.next
 find packages/fern-docs/bundle/.next -depth -mindepth 1 -not -path "packages/fern-docs/bundle/.next/standalone*" -exec rm -rf {} \;
-# cd ./packages/fern-docs/bundle
-# cp -r .next/static .next/standalone/packages/fern-docs/bundle/.next && cp -r public .next/standalone/packages/fern-docs/bundle
-# find .next -depth -mindepth 1 -not -path ".next/standalone*" -exec rm -rf {} \;
-# rm -rf .next/standalone/node_modules/.pnpm/esbuild@0.25.0 && rm -rf .next/standalone/node_modules/.pnpm/@esbuild+linux-x64@0.25.0
-# cd ../../../
-tar --no-xattrs -czf docs_bundle.tar.gz -C packages/fern-docs/bundle/.next/standalone .
-# Note: The following PowerShell Compress-Archive command (for Windows) will NOT preserve symlinks; 
-# any symlinks in the source directory will be included as regular files/directories (full copies of the targets).
-# powershell -Command "Compress-Archive -Path packages/fern-docs/bundle/.next/standalone\* -DestinationPath docs_bundle.zip"
+tar -czf docs_bundle.tar.gz -C packages/fern-docs/bundle/.next/standalone .
--- scripts/jsklan-zip-unzip-local.sh
+++
@@ -1,8 +0,0 @@
-rm docs_bundle.tar.gz
-tar -czf docs_bundle.tar.gz -C packages/fern-docs/bundle/.next/standalone .
-rm -rf .next
-mkdir .next
-cd .next
-mkdir standalone
-cd ..
-tar -xzf docs_bundle.tar.gz -C .next/standalone
