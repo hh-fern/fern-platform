a67f12e23d73dd14dacd53d1956f0a9a760f674e
Sahil <sahil@buildwithfern.com>
2025-09-18 17:32:22 -0400

    add sass() for scss

--- packages/search-widget/tsup.config.ts
+++ packages/search-widget/tsup.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "tsup";
+import { sassPlugin } from "esbuild-sass-plugin";
 
 export default defineConfig({
   entry: ["src/components/search.tsx"],
   format: ["esm", "cjs"],
   external: ["react", "react-dom", "next"],
+  esbuildPlugins: [sassPlugin()],
 });
