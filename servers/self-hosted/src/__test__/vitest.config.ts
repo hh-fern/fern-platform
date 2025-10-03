74b5487ece11511a39b8da782de8d415b245a56f
jsklan <jsklan.development@gmail.com>
2025-10-03 16:28:22 -0400

    format

--- servers/self-hosted/src/__test__/vitest.config.ts
+++ servers/self-hosted/src/__test__/vitest.config.ts
@@ -11,6 +11,10 @@ export default defineConfig({
             }
         },
         globalSetup: ["./src/__test__/setupSharedDocker.ts"],
-        include: ["./src/__test__/singleNode.test.ts", "./src/__test__/multiNode.test.ts", "./src/__test__/restrictedEnvironment.test.ts"]
+        include: [
+            "./src/__test__/singleNode.test.ts",
+            "./src/__test__/multiNode.test.ts",
+            "./src/__test__/restrictedEnvironment.test.ts"
+        ]
     }
 });
