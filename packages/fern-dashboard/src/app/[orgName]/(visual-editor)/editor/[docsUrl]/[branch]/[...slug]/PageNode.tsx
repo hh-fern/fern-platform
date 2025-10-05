81bc2b9046a0bf7975e0bfd89e231c1b9922a298
Andrew Jiang <andrewjiang@hey.com>
2025-10-05 04:35:06 -0400

    fix

--- packages/fern-dashboard/src/app/[orgName]/(visual-editor)/editor/[docsUrl]/[branch]/[...slug]/PageNode.tsx
+++ packages/fern-dashboard/src/app/[orgName]/(visual-editor)/editor/[docsUrl]/[branch]/[...slug]/PageNode.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import type { NodeId } from "@fern-api/fdr-sdk/navigation";
+import { CSSProvider } from "@fern-dashboard/visual-editor";
+import type { SerializableFoundNode } from "@fern-docs/components/navigation/types";
 import { SetCurrentNavigationNode } from "@fern-docs/components/state/navigation";
 import type { MdxToHtmlResponse } from "@fern-docs/mdx";
 import { useMemo } from "react";
-import { CSSProvider } from "@/components/editor/extension-custom-element/CSSContext";
 import { UnsupportedContent } from "@/components/editor/UnsupportedContent";
 import { usePages } from "@/providers/PagesStoreContext";
-
 import PageContents from "./PageContents";
 
 export declare namespace PageNode {
