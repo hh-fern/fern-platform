524c4339a911b9ef13096bea55a2c5441b449f04
Deep Singhvi <deep@buildwithfern.com>
2025-10-08 00:26:52 -0400

    Revert "Fix: Add editable layer for code block content"
    This reverts commit 2d100be65d406d7749d2ad3c8ff612856c979a0e.

--- packages/fern-dashboard/src/components/editor/extension-code-block/ShikiCodeBlockComponent.tsx
+++ packages/fern-dashboard/src/components/editor/extension-code-block/ShikiCodeBlockComponent.tsx
@@ -67,17 +67,7 @@ export function ShikiCodeBlockComponent(props: ReactNodeViewProps) {
                     </button>
                 </SearchableDropdown>
 
-                <div className="relative">
-                    {/* Shiki-highlighted preview layer */}
-                    <div className="pointer-events-none absolute inset-0" aria-hidden="true">
-                        <FernSyntaxHighlighter language={language} code={code} highlightLines={[]} highlightStyle="highlight" />
-                    </div>
-
-                    {/* Editable content layer */}
-                    <pre className="relative">
-                        <NodeViewContent as="code" className="opacity-0" />
-                    </pre>
-                </div>
+                <FernSyntaxHighlighter language={language} code={code} highlightLines={[]} highlightStyle="highlight" />
             </div>
         </NodeViewWrapper>
     );
